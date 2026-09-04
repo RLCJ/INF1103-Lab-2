932c7ed2d1f9bdc41a0b07d960bb783220df6a04
RLCJ <2601857@sit.singaporetech.edu.sg>
2026-09-04 14:59:32 +0800

    First Post

--- hello.py
+++ hello.py
@@ -7,6 +7,17 @@ username = "Cool Guy"
 bio = "I am a full time blogger!"
 followers = 100
 
+followers += 50  # Increase followers by 50
+print("Day 1 Update:", followers)
+
+followers += 20  # Increase followers by 20
+print("Day 2 Update:", followers)
+
+followers -= 10  # Decrease followers by 10
+print("Day 3 Update:", followers)
+
+
+
 print("Username:", username)
 print("Bio:", bio)
 print("Followers:", followers)
